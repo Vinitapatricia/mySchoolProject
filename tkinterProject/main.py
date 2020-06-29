faa2364aa107bc080752769387d84307262450e0
Vinitapatricia <vinitapatricia@gmail.com>
2020-06-29 18:08:11 +0700

    Tkiter Project

--- tkinterProject/main.py
+++ tkinterProject/main.py
@@ -9,23 +9,45 @@ myApps.title("Mt First Python Apps")
 myApps.resizable(True, True)
 
 #menambahkan label / tulisan
-label1 = ttk.Label(myApps, text = "Nama Lengkap \t :")
 #grid menampatkan object -> return none
+label1 = ttk.Label(myApps, text = "Nama Lengkap \t :")
 label1.grid(column = 0, row = 0)
+
 label2 = ttk.Label(myApps, text = "Tempat Lahir \t :")
 label2.grid(column = 0, row = 1)
+
 label3 = ttk.Label(myApps, text = "Tanggal Lahir \t :")
 label3.grid(column = 0, row = 2)
+
 label4 = ttk.Label(myApps, text = "Jenis Kelamin\t :")
 label4.grid(column = 0, row = 3)
-label5 = ttk.Label(myApps, text = "Alamat Tinggal\t :")
+
+label5 = ttk.Label(myApps, text = "Umur\t\t :")
 label5.grid(column = 0, row = 4)
 
 
 def changeColor():
+	label6 = ttk.Label(myApps, text = "Vinita Patricia")
+	label6.grid(column = 1, row = 0)
+	label7 = ttk.Label(myApps, text = "Palembang")
+	label7.grid(column = 1, row = 1)
+	label8 = ttk.Label(myApps, text = "23 Juni 2006")
+	label8.grid(column = 1, row = 2)
+	label9 = ttk.Label(myApps, text = "Perempuan")
+	label9.grid(column = 1, row = 3)
+	label10 = ttk.Label(myApps, text = "14\t ")
+	label10.grid(column = 1, row = 4)
 	button1.configure(text = "Color has been changed")
-	label1.configure(foreground = "blue")
-	label1.configure(text = "Nama Lengkap \t :")
+	label1.configure(foreground = "red")
+	label2.configure(foreground = "orange")
+	label3.configure(foreground = "yellow")
+	label4.configure(foreground = "green")
+	label5.configure(foreground = "blue")
+	label6.configure(foreground = "red")
+	label7.configure(foreground = "orange")
+	label8.configure(foreground = "yellow")
+	label9.configure(foreground = "green")
+	label10.configure(foreground = "blue")
 
 #membuat tombol / button
 button1 = ttk.Button(myApps, text = "Change Color", command = changeColor )
